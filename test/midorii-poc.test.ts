833a977b0d9a8f9601b5db2ef802309be43854b5
migitahiroaki <carp01hiro@gmail.com>
2024-07-08 22:39:36 +0900

    first commit

---
+++ test/midorii-poc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MidoriiPoc from '../lib/midorii-poc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/midorii-poc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MidoriiPoc.MidoriiPocStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
